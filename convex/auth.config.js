e6fca5af679855e784c64d1f6919646c1921924d
Zwelihle <zwelihle408@gmail.com>
2023-11-28 18:24:34 +0200

    new auth

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://summary-guinea-95.clerk.accounts.dev",
+      domain: "https://communal-cub-58.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
